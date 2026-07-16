7d70a5bee129d236298f22b0aa6bb253ec7fd683
Mya Schroder <schromya@gmail.com>
2026-07-15 20:26:00 -0500

    Corrected script to work with right glide

--- scripts/rivet_teleoperation.py
+++ scripts/rivet_teleoperation.py
@@ -23,12 +23,13 @@ import trossen_base
 
 
 ENABLE_RIGHT = True
-ENABLE_LEFT = True
+ENABLE_FOLLOWER = False
+ENABLE_LEFT = False
 ENABLE_BASE = True
 
 # IP addresses for the leader and follower
 IP_RIGHT_LEADER = "192.168.1.2"
-IP_RIGHT_FOLLOWER = "192.168.1.4"
+IP_RIGHT_FOLLOWER = "192.168.1.2"
 IP_LEFT_LEADER = "192.168.1.3"
 IP_LEFT_FOLLOWER = "192.168.1.5"
 
@@ -39,7 +40,7 @@ LEADER_OFFSET = 8.0      # N - leader opening offset
 
 BASE_MIN = -1           # Min translational/rotational velocity (m/s and rad/s)
 BASE_MAX = 1            # Max translational/rotational velocity (m/s and rad/s)
-BASE_LIFT_MAX = 500     # 10000   # Max lift velocity (motor units/s)
+BASE_LIFT_MAX = 8000     # 10000   # Max lift velocity (motor units/s)
 
 MIN_JOYSTICK = 0        # Joystick min value
 MAX_JOYSTICK = 4095     # Joystick max value
@@ -57,10 +58,12 @@ if __name__ == "__main__":
     print("Initializing the drivers...")
     if ENABLE_RIGHT:
         driver_right_leader = trossen_arm.TrossenArmDriver()
-        driver_right_follower = trossen_arm.TrossenArmDriver()
+        if ENABLE_FOLLOWER:
+            driver_right_follower = trossen_arm.TrossenArmDriver()
     if ENABLE_LEFT:
         driver_left_leader = trossen_arm.TrossenArmDriver()
-        driver_left_follower = trossen_arm.TrossenArmDriver()
+        if ENABLE_FOLLOWER:
+            driver_left_follower = trossen_arm.TrossenArmDriver()
     if ENABLE_BASE:
         base = trossen_base.TrossenBase()
 
@@ -68,37 +71,37 @@ if __name__ == "__main__":
 
     if ENABLE_RIGHT:
         driver_right_leader.configure(
-            trossen_arm.Model.wxai_v0,
+            trossen_arm.Model.glide_right,
             trossen_arm.StandardEndEffector.wxai_v0_leader,
             IP_RIGHT_LEADER,
             True,
         )
 
-        # TODO: SWAP OUT WITH GLIDE
-        driver_right_follower.configure(
-            trossen_arm.Model.wxai_v0,
-            trossen_arm.StandardEndEffector.wxai_v0_follower,
-            IP_RIGHT_FOLLOWER,
-            True,
-        )
+        if ENABLE_FOLLOWER:
+            driver_right_follower.configure(
+                trossen_arm.Model.wxai_v0,
+                trossen_arm.StandardEndEffector.wxai_v0_follower,
+                IP_RIGHT_FOLLOWER,
+                True,
+            )
 
     if ENABLE_LEFT:
         driver_left_leader.configure(
-            trossen_arm.Model.wxai_v0,
+            trossen_arm.Model.glide_left,
             trossen_arm.StandardEndEffector.wxai_v0_leader,
             IP_LEFT_LEADER,
             True,
         )
 
-        # TODO: SWAP OUT WITH GLIDE
-        driver_left_follower.configure(
-            trossen_arm.Model.wxai_v0,
-            trossen_arm.StandardEndEffector.wxai_v0_follower,
-            IP_LEFT_FOLLOWER,
-            True,
-        )
+        if ENABLE_FOLLOWER:
+            driver_left_follower.configure(
+                trossen_arm.Model.wxai_v0,
+                trossen_arm.StandardEndEffector.wxai_v0_follower,
+                IP_LEFT_FOLLOWER,
+                True,
+            )
 
-    if ENABLE_RIGHT:
+    if ENABLE_RIGHT and ENABLE_FOLLOWER:
         motor_parameters = driver_right_follower.get_motor_parameters()
         motor_parameters[0][trossen_arm.Mode.position].velocity.ki = 0.0
         motor_parameters[0][trossen_arm.Mode.position].velocity.imax = 0.0
@@ -119,7 +122,7 @@ if __name__ == "__main__":
         motor_parameters[6][trossen_arm.Mode.position].velocity.ki = 0.0
         driver_right_follower.set_motor_parameters(motor_parameters)
 
-    if ENABLE_LEFT:
+    if ENABLE_LEFT and ENABLE_FOLLOWER:
         motor_parameters = driver_left_follower.get_motor_parameters()
         motor_parameters[0][trossen_arm.Mode.position].velocity.ki = 0.0
         motor_parameters[0][trossen_arm.Mode.position].velocity.imax = 0.0
@@ -142,17 +145,19 @@ if __name__ == "__main__":
 
     if ENABLE_RIGHT:
         driver_right_leader.set_all_modes(trossen_arm.Mode.external_effort)
-        driver_right_follower.set_all_modes(trossen_arm.Mode.position)
-
         driver_right_leader.set_gripper_mode(trossen_arm.Mode.external_effort)
-        driver_right_follower.set_gripper_mode(trossen_arm.Mode.position)
+
+        if ENABLE_FOLLOWER:
+            driver_right_follower.set_all_modes(trossen_arm.Mode.position)
+            driver_right_follower.set_gripper_mode(trossen_arm.Mode.position)
 
     if ENABLE_LEFT:
         driver_left_leader.set_all_modes(trossen_arm.Mode.external_effort)
-        driver_left_follower.set_all_modes(trossen_arm.Mode.position)
-
         driver_left_leader.set_gripper_mode(trossen_arm.Mode.external_effort)
-        driver_left_follower.set_gripper_mode(trossen_arm.Mode.position)
+
+        if ENABLE_FOLLOWER:
+            driver_left_follower.set_all_modes(trossen_arm.Mode.position)
+            driver_left_follower.set_gripper_mode(trossen_arm.Mode.position)
 
     print("Moving to home positions...")
 
@@ -162,12 +167,12 @@ if __name__ == "__main__":
 
 
     # Follower gripper is offset by -pi/4 to match the leader's gripper position at home
-    if ENABLE_RIGHT:
+    if ENABLE_RIGHT and ENABLE_FOLLOWER:
         driver_right_follower.set_all_positions(
             np.array([0.0, 0.0, 0.0, 0.0, 0.0, np.pi / 4, 0.0]), 1.0, True
         )
 
-    if ENABLE_LEFT:
+    if ENABLE_LEFT and ENABLE_FOLLOWER:
         driver_left_follower.set_all_positions(
             np.array([0.0, 0.0, 0.0, 0.0, 0.0, -np.pi / 4, 0.0]), 1.0, True
         )
@@ -186,31 +191,40 @@ if __name__ == "__main__":
 
             ################################### LEADER COMMANDS ####################################
             if ENABLE_RIGHT:
-                right_efforts = driver_right_follower.get_all_external_efforts()
                 right_positions = driver_right_leader.get_all_positions()
-                right_input = driver_right_follower.get_input_report()
-
+                right_input = driver_right_leader.get_input_report()
+                if ENABLE_FOLLOWER:
+                    right_efforts = driver_right_follower.get_all_external_efforts()
                 # Scale to -1 to 1 velocity (rad/s)
                 base_velocity_angular_z = scale(right_input.joystick_x, MIN_JOYSTICK, MAX_JOYSTICK,
                                 BASE_MIN, BASE_MAX)
 
+                # Button bitmap: bit n is SEL_(n+1), 1 is pressed:
+                #   [SEL_1, SEL_2, SEL_3, SEL_4, SEL_5]
+                left_up_btn = int(right_input.buttons & (1 << 0))
+                left_down_btn = int(right_input.buttons & (1 << 2))
+                base_velocity_lift = int(left_up_btn - left_down_btn) * BASE_LIFT_MAX
+
+                # TODO: @schromya Move to other controller when available
+                # TODO: See why negative needed
+                base_velocity_linear_y = -scale(right_input.joystick_y, MIN_JOYSTICK, MAX_JOYSTICK,
+                                BASE_MIN, BASE_MAX)
+
 
             if ENABLE_LEFT:
-                left_efforts = driver_left_follower.get_all_external_efforts()
                 left_positions = driver_left_leader.get_all_positions()
-                left_input = driver_left_follower.get_input_report()
+                left_input = driver_left_leader.get_input_report()
+
+                if ENABLE_FOLLOWER:
+                    left_efforts = driver_left_follower.get_all_external_efforts()
 
                 # Scale to -1 to 1 velocity (m/s)
                 base_velocity_linear_x = scale(left_input.joystick_x, MIN_JOYSTICK, MAX_JOYSTICK,
                                                BASE_MIN, BASE_MAX)
-                base_velocity_linear_y = scale(left_input.joystick_y, MIN_JOYSTICK, MAX_JOYSTICK,
-                                               BASE_MIN, BASE_MAX)
+                # base_velocity_linear_y = scale(left_input.joystick_y, MIN_JOYSTICK, MAX_JOYSTICK,
+                #                                BASE_MIN, BASE_MAX)
 
-                # Button bitmap: bit n is SEL_(n+1), 1 is pressed:
-                #   [SEL_1, SEL_2, SEL_3, SEL_4] = [???]
-                left_up_btn = int(left_input.buttons & (1 << 1)) # TODO: Check these
-                left_down_btn = int(left_input.buttons & (1 << 2))
-                base_velocity_lift = int(left_up_btn - left_down_btn) * BASE_LIFT_MAX
+
 
             ###################################### FOLLOWERS #######################################
             if ENABLE_BASE:
@@ -220,7 +234,7 @@ if __name__ == "__main__":
                 base.set_actuator_velocity(base_velocity_lift)
 
             # RIGHT ARM
-            if ENABLE_RIGHT:
+            if ENABLE_RIGHT and ENABLE_FOLLOWER:
                 # Feed the external efforts from the follower robot to the leader robot
                 driver_right_leader.set_arm_external_efforts(
                     np.array(
@@ -254,9 +268,10 @@ if __name__ == "__main__":
                 )
 
                 # RIGHT ARM GRIPPER
-
                 # Read the leader's gripper position and follower's gripper effort
                 leader_right_position = driver_right_leader.get_gripper_position()
+
+
                 follower_right_effort = driver_right_follower.get_gripper_effort()
 
                 # Cubic curve for more resistance at higher efforts and less at lower efforts, with an offset to keep the gripper open when not gripping
@@ -268,6 +283,7 @@ if __name__ == "__main__":
                     leader_right_effort, 0.2, False
                 )
 
+
                 # Set the follower's gripper position to match the leader's position
                 driver_right_follower.set_gripper_position(
                     leader_right_position, 0.0, False
@@ -277,7 +293,7 @@ if __name__ == "__main__":
                 follower_right_position = driver_right_follower.get_gripper_position()
 
             # LEFT ARM
-            if ENABLE_LEFT:
+            if ENABLE_LEFT and ENABLE_FOLLOWER:
                 # Feed the external efforts from the follower robot to the leader robot
                 driver_left_leader.set_arm_external_efforts(
                     np.array(
@@ -293,6 +309,7 @@ if __name__ == "__main__":
                     0.0,
                     False,
                 )
+
 
                 # Feed the positions from the leader robot to the follower robot
                 driver_left_follower.set_arm_positions(
@@ -311,7 +328,6 @@ if __name__ == "__main__":
                 )
 
                 # LEFT ARM GRIPPER
-
                 # Read the leader's gripper position and follower's gripper effort
                 leader_left_position = driver_left_leader.get_gripper_position()
                 follower_left_effort = driver_left_follower.get_gripper_effort()
@@ -335,13 +351,13 @@ if __name__ == "__main__":
 
     except KeyboardInterrupt:
         print("Moving to stop positions...")
-        if ENABLE_RIGHT:
+        if ENABLE_RIGHT and ENABLE_FOLLOWER:
             driver_right_follower.set_all_modes(trossen_arm.Mode.position)
             driver_right_follower.set_all_positions(
                 np.zeros(driver_right_follower.get_num_joints()), 2.0, True
             )
 
-        if ENABLE_LEFT:
+        if ENABLE_LEFT and ENABLE_FOLLOWER:
             driver_left_follower.set_all_modes(trossen_arm.Mode.position)
             driver_left_follower.set_all_positions(
                 np.zeros(driver_left_follower.get_num_joints()), 2.0, True
